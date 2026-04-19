805c519e998543eab5bc707ef1bc2a1de5debc92
Liz <lizrd2016@gmail.com>
2026-04-18 17:13:23 -0700

    add GitHub pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+const repository = import.meta.env.GITHUB_REPOSITORY || '';
+const repositoryName = repository.split('/')[1] || '';
+const owner = import.meta.env.GITHUB_REPOSITORY_OWNER || '';
+const githubPagesUrl = import.meta.env.GITHUB_PAGES_URL || '';
+const isUserSite = repositoryName.endsWith('.github.io');
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: githubPagesUrl || (owner ? `https://${owner}.github.io` : 'http://localhost:4321'),
+	base: repositoryName && !isUserSite ? `/${repositoryName}` : '/',
+});
