ee6c79e632213f37544fb6f371ff6d96603d7da5
Liz <lizrd2016@gmail.com>
2026-04-18 17:16:44 -0700

    revert to old config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
-export default defineConfig({
-  site: 'https://YOUR_USERNAME.github.io',
-  base: '/portfolio',
-});
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({});
